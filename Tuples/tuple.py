3a3cd27d30258f330299a956f8f6d4f63866c5a5
happy2111 <muhammadyusufa2111@gmail.com>
2024-10-24 21:33:49 +0500

    Tuples

---
+++ Tuples/tuple.py
@@ -0,0 +1,78 @@
+# # Task 1
+# # ---1
+# fruits = ('apple', 'banana','cherry', 'orange', 'kiwi')
+# # ---2
+# first_last = fruits[0::4]
+# print(first_last)
+# # --3
+# secondElem = fruits[2]
+# print(secondElem)
+
+
+
+# # Task 2
+# # ---1
+# numbers = (1,2,3,4,5,6,2)
+# # ---2
+# count = numbers.count(2)
+# print(count)
+# # ---3
+# index = numbers.index(5)
+# print(index)
+
+
+# # Task 3
+# # ---1
+# colors = ('red', 'green', 'blue')
+# # ---2
+# colors = list(colors)
+# colors.append('yellow')
+# # ---3
+# colors = tuple(colors)
+# print(colors)
+
+
+# # Task 4
+# # ---1
+# letters = ('a', 'b', 'c', 'd', 'e')
+# # ---2
+# letters = list(letters)
+# letters.reverse()
+# letters = tuple(letters)
+# print(letters)
+
+
+
+# # Task 5 
+# # ---1
+# nested_tuple = (1,2,(3,4,5),6,7)
+# # ---2
+# second_second = nested_tuple[2]
+# print(second_second)
+
+
+
+# # Task 6 
+# # --1
+# my_tuple = (10, 20, 30, 40, 50)
+# # ---2
+# my_tuple = list(my_tuple)
+# my_tuple.append(60)
+# # ---3
+# my_tuple = tuple(my_tuple)
+# print(my_tuple)
+
+
+# # Task 7 
+# # ---1
+# tuple1 = (1, 2, 3)
+# tuple2 = (4, 5, 6)
+# # ---2
+# tuple1 = list(tuple1)
+# tuple2 = list(tuple2)
+# tuple1.extend(tuple2)
+# tuple1 = tuple(tuple1)
+# print(tuple1)
+
+
+
